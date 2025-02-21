dbb965bec2401fd1e6eceb8b5bc16a914fb3ba66
TonyBllvc <george.ekwemadu@gmail.com>
2025-02-21 07:02:30 +0100

    events and conditional rendering

--- app.js
+++
@@ -1,7 +0,0 @@
-const app = Vue.createApp({
-    // Data, functions, 
-    // template: '<h2>I am a template</h2>'
-})
-
-app.mount('#app')
-
---
+++ events/others/app.js
@@ -0,0 +1,31 @@
+const app = Vue.createApp({
+    // Data, functions,
+    // template: '<h2>I am a template</h2>'
+    // to output a dynamic value int=side our template, we have to use:
+    // data function: Defining it the way you would within an object..
+    // .. then return the content you wish to display, i.e. "title"
+    data() {
+        // Data template
+        return {
+            showBooks: true,
+            title: "The final empire",
+            author: "Black man",
+            age: 25
+        }
+    },
+    methods: {
+        toogleBook() {
+            this.showBooks = !this.showBooks
+        },
+
+        handleEvent(e, data) {
+            console.log(e.type)
+            if (data) {
+                console.log(data)
+            }
+        }
+    }
+})
+
+app.mount('#app')
+
