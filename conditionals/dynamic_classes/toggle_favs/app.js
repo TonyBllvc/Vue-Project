85ec15afbfa1b0c9d7e208ecb626f25691fb9b1a
TonyBllvc <george.ekwemadu@gmail.com>
2025-02-22 23:42:42 +0100

    conditionals

--- conditionals/dynamic_classes/toggle_favs/app.js
+++ conditionals/dynamic_classes/toggle_favs/app.js
@@ -2,12 +2,11 @@ const app = Vue.createApp({
     data() {
         // Data template
         return {
-            url: 'https://www.netninja.co.uk',
             showBooks: true,
             books: [
-                { title: 'Name of the Wind', author: 'Patrick Rothfuss', isFav: true, img: '../assets/Screenshot (1).png', id: 1 },
-                { title: 'The Way of Kings', author: 'Brandon Sanderson', img: '../assets/Screenshot (11).png', isFav: false, id: 2 },
-                { title: 'Good Omens', author: 'Terry Pratchett & Neil Gaiman', img: '../assets/Screenshot (14).png', isFav: true, id: 3 }
+                { title: 'Name of the Wind', author: 'Patrick Rothfuss', isFav: true, img: '../../../assets/Screenshot (1).png', id: 1 },
+                { title: 'The Way of Kings', author: 'Brandon Sanderson', img: '../../../assets/Screenshot (11).png', isFav: false, id: 2 },
+                { title: 'Good Omens', author: 'Terry Pratchett & Neil Gaiman', img: '../../../assets/Screenshot (14).png', isFav: true, id: 3 }
             ]
         }
     },
@@ -15,7 +14,10 @@ const app = Vue.createApp({
         toogleBook() {
             this.showBooks = !this.showBooks
         },
-
+        toggleFav(book) {
+            book.isFav = !book.isFav
+            
+        },
         handleEvent(e, data) {
             console.log(e.type)
             if (data) {
