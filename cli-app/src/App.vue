57fc46e7d026b4f89622b5017bd8338b49b6c71a
TonyBllvc <george.ekwemadu@gmail.com>
2025-02-24 23:05:38 +0100

    My first edit in vue

--- cli-app/src/App.vue
+++ cli-app/src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2>{{ title }}</h2>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      title: 'My firts vue app'
+    }
   }
 }
+
 </script>
 
 <style>
@@ -22,5 +23,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h2{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
