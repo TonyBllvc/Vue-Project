c96648d95e882a2f694695db9597ffb6cec91a9a
TonyBllvc <george.ekwemadu@gmail.com>
2025-03-09 17:32:33 +0100

    completed

--- reaction_timer_App/src/components/Results.vue
+++ reaction_timer_App/src/components/Results.vue
@@ -32,10 +32,10 @@
 </script>
 
 <style>
-.rank{
-    font-size: 1.4em;
-    color: #0faf87;
-    font-weight: bold;
-}
+    .rank {
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
 
 </style>
