def0322331bb374764131fc7dc89b84b154c6d19
TonyBllvc <george.ekwemadu@gmail.com>
2025-03-09 17:32:12 +0100

    I copleted my react-timer project

--- reaction_timer_App/src/App.vue
+++ reaction_timer_App/src/App.vue
@@ -1,25 +1,40 @@
 <template>
   <h1> Reaction timer</h1>
-  <!-- v-bind:disabled: lets use disable the button if criteriates meet -->
+  <!-- @click or v-on:click: Lets me access my click event in my javascript -->
+  <!-- v-bind:disabled or :disbaled: lets use disable the button if criteriates meet -->
   <button @click="start" v-bind:disabled="isPlaying">Play</button>
 
-  <Block v-if="isPlaying" v-bind:delay="delay" />
+
+  <!-- <div v-show="showShadow" class="box-touch">
+  </div> -->
+  <!-- v-bind:delay or :delay: used as a props in this case -->
+  <Block v-if="isPlaying" v-bind:delay="delay" @end="endGame" />
+
+  <Results v-show="showResults" v-bind:result="score" />
+  <!-- <p v-show="showResults">
+    Reaction time: {{ score }} ms
+  </p> -->
 </template>
 
 <script>
   import Block from './components/Block.vue'
+  import Results from './components/Results.vue'
 
   export default {
 
     name: 'App',
     components: {
-      Block
+      Block,
+      Results
 
     },
     data() {
       return {
         isPlaying: false,
         delay: null,
+        score: null,
+        showResults: false,
+        // showShadow: true
       }
     },
     methods: {
@@ -34,7 +49,13 @@
           So, the total delay will be a random time between 2000 milliseconds (2 seconds) and 7000 milliseconds (7 seconds).
         */
         this.isPlaying = true
-        console.log(this.delay)
+        this.showResults = false
+        // console.log(this.delay)
+      },
+      endGame(reactionTime) {
+        this.score = reactionTime
+        this.isPlaying = false
+        this.showResults = true
       }
     }
 
@@ -51,5 +72,31 @@
     text-align: center;
     color: #444;
     margin-top: 60px;
+  }
+  button{
+    background-color: #0faf87;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    margin: 10px;
+  }
+  button[disabled]{
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
+  .box-touch {
+    width: 400px;
+    border-radius: 20px;
+    border: #c4c4c4 solid 1px;
+    background-color: #f6f5f5;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+
   }
 </style>
--- reaction_timer_App/src/components/Block.vue
+++ reaction_timer_App/src/components/Block.vue
@@ -1,12 +1,53 @@
 <template>
-    <div class="block">
+    <div class="block" v-if="showBlock" v-on:click="stopTimer">
         click me
     </div>
 </template>
 
 <script>
     export default {
-        props: ['delay']
+        props: ['delay'],
+        data() {
+            return {
+                showBlock: false,
+                timer: null,
+                reactionTime: 0
+            }
+        },
+        // when component has mounted DOM
+        mounted() {
+            // console.log('component mounted')
+            setTimeout(() => {
+                this.showBlock = true
+                this.starTimer()
+                // console.log(this.delay)
+            }, this.delay)
+        },
+        methods: {
+            starTimer() {
+                this.timer = setInterval(() => {
+                    this.reactionTime += 10
+                }, 10)
+            },
+            stopTimer() {
+                clearInterval(this.timer)
+                // (function props, data sent(this will be a parameter within the "end" function) )
+                this.$emit('end', this.reactionTime)
+                console.log(this.reactionTime)
+            }
+        }
+        // beforeUpdate() {
+        //   console.log('component before update')  
+        // },
+        // // used when data is updated
+        // updated() {
+        //     console.log('component updated')
+        // },
+        // // used when component has been unmounted from DOM
+        // unmounted() {
+        //     console.log('component unmounted')
+        // }, 
+        
     }
 </script>
 
