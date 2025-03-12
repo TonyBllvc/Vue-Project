33827b815a61e85b400ead47ef3b4fa4b956903f
TonyBllvc <george.ekwemadu@gmail.com>
2025-03-12 22:35:40 +0100

    form

--- web_form/src/components/SignupForm.vue
+++ web_form/src/components/SignupForm.vue
@@ -13,12 +13,25 @@
                 <option value="designer">Web Designer</option>
             </select>
 
+            <label>Skills</label>
+            <input type="text" v-model="tempSkill" v-on:keyup="addSkill">
+
+            <div v-for="skill in skills" :key="skill" class="pill">
+                <span v-on:click="deleteSkill(skill)">
+                    {{ skill }} 
+                </span>
+            </div>
+
             <div class="terms">
                 <input type="checkbox" v-model="terms" required>
                 <label> Accept terms and conditions</label>
             </div>
+
+            <div class="submit">
+                <button> Create an Account </button>
+            </div>
         </div>
-<!-- 
+        <!-- 
         <div>
             <input type="checkbox" value="Shaun" v-model="names">
             <label> Shaun </label>
@@ -47,9 +60,32 @@
                 password: '',
                 role: this.role || "",
                 terms: false,
+                tempSkill: '',
+                skills: []
                 // names: []
             }
         },
+        methods: {
+            addSkill(e) {
+                console.log(e)
+                if ((',' == e.key || "Enter" == e.key) && this.tempSkill) {
+                    let skill = this.tempSkill.split(',')[0].trim();
+                    if (skill && !this.skills.includes(skill)) {
+                        this.skills.push(skill);
+                        // original code
+                        // if (!this.skills.includes(this.tempSkill)) {
+                        //     this.skills.push(this.tempSkill).slice(0, 1)
+                    }
+                    this.tempSkill = ''
+                }
+            },
+            deleteSkill(skill) {
+                this.skills = this.skills.filter((e) => {
+                    return e !== skill
+                })
+            }
+
+        }
 
 
     }
@@ -105,5 +141,37 @@
         position: relative;
         top: 2px;
     }
-
+    .pill{
+        display: inline-block;
+        margin: 10px 10px 0 0;
+        padding: 6px 12px;
+        background-color: #eee;
+        border-radius: 20px;
+        font-size: 12px;
+        letter-spacing: 1px;
+        font-weight: bold;
+        color: #777;
+        cursor: pointer;
+    }
+    button{
+        background-color: #0b6dff;
+        border: 0;
+        padding: 10px 20px;
+        margin-top: 20px;
+        color: white;
+        border-radius: 20px;
+    }
+    .submit{
+        text-align: center;
+    }
+    /* .clear {
+        border: none;
+        outline: none;
+        color: red;
+        background-color: transparent;
+        font-size: 10px;
+        font-weight: 700;
+        position: relative;
+        display: inline-flex;
+    } */
 </style>
